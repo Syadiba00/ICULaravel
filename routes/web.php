3da75fc0cbb6922523fa0d87073aa4a25ddf128c
syadiba <syadiba.zaimi@icu.gov.my>
2024-10-15 10:38:07 +0800

    1. Made changes on title based on file name - file name is based on env file. 2. Made changes on passing parameter

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,61 @@ Route::get('/home', function () {
 Route::get('/auth/signin', function () {
     return view('auth.signin');
 });
+
+//Route Param
+Route ::get('/user/{name}/age{age}', function ($name,$age) {
+    return 'User'.$name. 'age is: '.$age;
+} );
+
+Route ::get('/user/{name}}', function ($name) {
+    return 'User'.$name;
+} );
+
+//display name dekat home.blade using parameter
+Route ::get('/home/{name}', function ($name) {
+    return view('home', ['name'=> $name]);
+});
+
+//Named route
+Route:: get('/user/profile', function () {
+    return 'Pengguna Profiles';
+}) ->name('user.profile');
+
+//??
+Route ::get('/user/{name})', function ($name) {
+    return 'User'.$name;
+} );
+
+//alias of a route user.profile = /pengguna/profile]
+
+//redirect route to named route
+Route:: get('\redirect-to-profile', function () {
+    return 'Pengguna Profiles';
+}) ->name('user.profile');
+
+//route group - without type satu2
+Route::prefix('food')->group(function () {
+
+    Route::get('/details', function () {
+        return 'Food details are following';
+    });
+
+    Route::get('/home', function () {
+        return 'Food home page';
+    });
+    
+});
+
+//combination of all above(Route,Named Route, Route Param, Route Group, Route Prefix)
+Route::name('job')->prefix('job')-> group (function () {
+
+    Route::get('home', function () {
+        return 'Job Home Page';
+    }) ->name('.home');
+
+    Route::get('/details', function () {
+        return 'Job details are following';
+    })->name('.description');
+
+});
+    
